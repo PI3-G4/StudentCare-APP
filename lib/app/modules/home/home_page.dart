5a7ab678cde9d8d55b87fbb4ed671dac598cf8e7
Renato Ferreira <Renatoawk@gmail.com>
2020-09-02 12:15:00 -0300

    Login - Algumas correções

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'home_controller.dart';
+import 'package:studentcare/app/modules/widgets/input_field.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -16,11 +18,57 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
+      backgroundColor: Colors.grey[850],
+      body: Observer(
+        builder: (_) => Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(),
+            SingleChildScrollView(
+              child: Container(
+                margin: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Icon(
+                      Icons.store_mall_directory,
+                      color: Colors.pinkAccent,
+                      size: 160,
+                    ),
+                    InputField(
+                      icon: Icons.person_outline,
+                      hint: "Usuário",
+                      obscure: false,
+                    ),
+                    InputField(
+                      icon: Icons.lock_outline,
+                      hint: "Senha",
+                      obscure: true,
+                    ),
+                    RaisedButton(
+                      color: Colors.pinkAccent,
+                      child: Text("Entrar"),
+                      onPressed: () {},
+                      textColor: Colors.white,
+                    ),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    SizedBox(
+                      height: 50,
+                      child: RaisedButton(
+                        color: Colors.pinkAccent,
+                        child: Text("Cadastrar"),
+                        onPressed: () {},
+                        textColor: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
